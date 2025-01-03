02d6b2297631f73b2a3a4d5cb96437622e65e084
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-03 18:50:45 +0530

    ok

---
+++ 02--variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1 # no space before and after equal
+PERSON2=$2
+person3=$3
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hello $PERSON1"
+echo "$PERSON1: How are you man"
+echo "$PERSON2 : I am good, what about you"
+echo "$PERSON1: Where is $person3"
