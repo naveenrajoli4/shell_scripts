f54ac542bca76f2571875de28d71ae826fd3f338
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-07 10:34:59 +0530

    backups&unzip

---
+++ 18-backups.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+SOURCE_DIR=$1
+DESTINATION_DIR=$2
+DAY=${3:-14}
+
+LOGS_FOLDER="/home/ec2-user/shell_scripts"
+LOG_FILE=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%d-%m-%Y-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP"
+
+if [ $# -lt 2 ]
+then 
+    echo "USEAGE: sh script_name <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    exit 1
+fi
+
+if [ ! -d $SOURCE_DIR ]
+then
+    echo "destinantion directory not exist"
+    exit 1
+fi
+
+if [ ! -d $DESTINATION_DIR ]
+then
+    echo "destinantion directory not exist"
+    exit 1   
+fi
+
+echo "Script started executing at: $TIMESTAMP"
+
+FILES=$(find $SOURCE_DIR -name "*.log")
+
+if [ -n "$FILES" ] 
+then 
+    echo "Files to zip are:$FILES"
+    ZIP_FILES="$DESTINATION_DIR/why-$TIMESTAMP.zip"
+    find $SOURCE_DIR -name "*.log" | zip -@ "$ZIP_FILES"
+else
+    echo "no files older than $DAY to delete"
+    exit 1
+fi
+
+ZIP_FILES="$DESTINATION_DIR/why-$TIMESTAMP.zip"
+find $SOURCE_DIR -name "*.log" | zip -@ "$ZIP_FILES"
+
+# if [ -f $ZIP_FILES ]
+# then 
+    echo "successfully created zip "
+else 
+    echo "zipping error "
+    exit 1
+fi
+
+while read -r $filesd
+do  
+    echo "deleting files : $filesd" 
+    rm -rf $filesd
+    "echo deleted files: $filesd"
+done <<< $FILES
