930ad56b4f809385c6b0e2d50c56e3044e699b55
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-07 11:15:30 +0530

    backups&unzip

--- 18-backups.sh
+++ 18-backups.sh
@@ -53,16 +53,6 @@ fi
 
 echo "Script started executing at: $TIMESTAMP"
 
-FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
-
-if [ -n "$FILES" ] 
-then 
-    echo "Files to zip are:$FILES"
-    else
-    echo "no files older than $DAYS to delete"
-    exit 1
-fi
-
 dnf list installed zip
 if [ $? -ne 0 ]
 then
@@ -76,6 +66,16 @@ then
     fi
 else
     echo "ZIP is allready installed "
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
+
+if [ -n "$FILES" ] 
+then 
+    echo "Files to zip are:$FILES"
+    else
+    echo "no files older than $DAYS to delete"
+    exit 1
+fi
 
 ZIP_FILES="$DESTINATION_DIR/sorc_01-$TIMESTAMP.zip"
 find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILES"
