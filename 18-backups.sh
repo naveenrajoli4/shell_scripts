6f4e20fc71dc4ee6724dad0318fc15899582220a
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-07 11:45:09 +0530

    backups&unzip

--- 18-backups.sh
+++ 18-backups.sh
@@ -4,16 +4,18 @@ SOURCE_DIR=$1
 DESTINATION_DIR=$2
 DAYS=${3:-14}
 
-LOGS_FOLDER="/home/ec2-user/shell_scripts"
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%d-%m-%Y-%H-%M-%S)
-LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP"
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
 if [ $# -lt 2 ]
 then 
     echo "USEAGE: sh script_name <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
     exit 1
 fi
+
+mkdir -p /home/ec2-user/shellscript-logs
 
 if [ ! -d $SOURCE_DIR ]
 then
@@ -33,6 +35,7 @@ fi
 
 echo "Script started executing at: $TIMESTAMP"
 
+
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +$DAYS)
 
 if [ -n "$FILES" ] 
@@ -43,16 +46,16 @@ then
     exit 1
 fi
 
-ZIP_FILES="$DESTINATION_DIR/sorc_01-$TIMESTAMP.zip"
-find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILES"
+# ZIP_FILES="$DESTINATION_DIR/backuplogs-$TIMESTAMP.zip"
+# find $SOURCE_DIR -name "*.log" -mtime +$DAYS | zip -@ "$ZIP_FILES"
 
-if [ -f "$ZIP_FILES" ]
-then 
-    echo "Successfully created zip file for files older than $DAYS"
-else 
-    echo "zipping error"
-    exit 1
-fi
+# if [ -f "$ZIP_FILES" ]
+# then 
+#     echo "Successfully created zip file for files older than $DAYS"
+# else 
+#     echo "zipping error"
+#     exit 1
+# fi
 
 # while read -r filepath # here filepath is the variable name, you can give any name
 # do
