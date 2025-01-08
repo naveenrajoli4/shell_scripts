82c333fb5ec5c3bdad436b8cfa38564bd470051d
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-08 18:33:01 +0530

    ok

--- 19.diskusege.sh
+++ 19.diskusege.sh
@@ -1,23 +1,23 @@
 # #!/bin/bash
 
-# DISK_USAGE=$(df -hT | grep xfs)
-# THRESHOLD_LIMIT="5" #real projects will monitor for 70
-
-# while read -r lines
-# do
-#     USEAGE=$(echo $lines | awk -F " " '{print $6f}')
-#     PARTITION=$(echo $lines | awk -F " " '{print $Nf}')
-#     echo "partion : $PARTITION , usege :  $USEAGE "
-
-# done <<< $DISK_USAGE
-
 DISK_USAGE=$(df -hT | grep xfs)
-DISK_THRESHOLD=5 #real projects will monitor for 70
-MSG=""
+THRESHOLD_LIMIT="5" #real projects will monitor for 70
 
-while read -r line
+while read -r lines
 do
-    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-    PARTITION=$(echo $line | awk -F " " '{print $NF}')
-    echo "Partition: $PARTITION , Usage: $USAGE"
+    USEAGE=$(echo $lines | awk -F " " '{print $6f}' | cut -d "%" -f1)
+    PARTITION=$(echo $lines | awk -F " " '{print $Nf}')
+    echo "partion : $PARTITION , usege :  $USEAGE "
+
 done <<< $DISK_USAGE
+
+# DISK_USAGE=$(df -hT | grep xfs)
+# DISK_THRESHOLD=5 #real projects will monitor for 70
+# MSG=""
+
+# while read -r line
+# do
+#     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+#     PARTITION=$(echo $line | awk -F " " '{print $NF}')
+#     echo "Partition: $PARTITION , Usage: $USAGE"
+# done <<< $DISK_USAGE
