b4c48c17bd68384f5e74ac4abdc5ef1b43453695
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-08 18:46:53 +0530

    ok

--- 19.diskusege.sh
+++ 19.diskusege.sh
@@ -5,7 +5,7 @@ DISK_THRESHOLD=5 #real projects will monitor for 70
 
 while read -r line
 do
-    USAGE=$(echo $line | awk -F " " '{print $6f}')
+    USAGE=$(echo $line | awk -F " " '{print $6f}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
     echo "Partition: $PARTITION , Usage: $USAGE"
 done <<< $DISK_THRESHOLD
