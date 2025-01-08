dfb8113f99ccece333e4d17ddab7c824bac3b056
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-08 18:31:26 +0530

    ok

--- 19.diskusege.sh
+++ 19.diskusege.sh
@@ -1,12 +1,23 @@
-#!/bin/bash
+# #!/bin/bash
+
+# DISK_USAGE=$(df -hT | grep xfs)
+# THRESHOLD_LIMIT="5" #real projects will monitor for 70
+
+# while read -r lines
+# do
+#     USEAGE=$(echo $lines | awk -F " " '{print $6f}')
+#     PARTITION=$(echo $lines | awk -F " " '{print $Nf}')
+#     echo "partion : $PARTITION , usege :  $USEAGE "
+
+# done <<< $DISK_USAGE
 
 DISK_USAGE=$(df -hT | grep xfs)
-THRESHOLD_LIMIT="5" #real projects will monitor for 70
+DISK_THRESHOLD=5 #real projects will monitor for 70
+MSG=""
 
-while read -r lines
+while read -r line
 do
-    USEAGE=$(echo $lines | awk -F " " '{print $6f}')
-    PARTITION=$(echo $lines | awk -F " " '{print $Nf}')
-    echo "partion : $PARTITION , usege :  $USEAGE "
-
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk -F " " '{print $NF}')
+    echo "Partition: $PARTITION , Usage: $USAGE"
 done <<< $DISK_USAGE
