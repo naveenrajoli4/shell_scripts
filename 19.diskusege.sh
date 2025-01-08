4c9f158288f2ea0600f03c479f84ead11967c00c
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-08 18:20:41 +0530

    ok

--- 19.diskusege.sh
+++ 19.diskusege.sh
@@ -6,5 +6,7 @@ THRESHOLD_LIMIT="5" #real projects will monitor for 70
 while read -r lines
 do
     USEAGE=$(echo $line | awk -F " " {print $6f} | cut -d "%" -f1)
-    
+    PARTITION=$(echo $line | awk -F " " {print $Nf})
+    echo "partion : $PARTITION  uusahe :  $USEAGE "
+
 done <<< $DISK_USAGE
