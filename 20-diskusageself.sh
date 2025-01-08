ec654a39242dd922f713653d9cb200d6d6b40107
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-08 19:14:34 +0530

    ok

--- 20-diskusageself.sh
+++ 20-diskusageself.sh
@@ -2,11 +2,17 @@
 
 DISKS=$(df -hT | grep xfs)
 LIMIT=5
+MSG=""
 
 while read -r line
 do 
     USE=$(echo "$line" | awk -F " " '{print $6F}')
     PART=$(echo "$line" | awk -F " " '{print $NF}')
-    echo "use $USE , part: $PART"
-
+    echo "part: $PART , use: $USE"
+     if [ $USE -ge $LIMIT ]
+     then
+        MSG=""High Disk usage on partition: $PART Usage is: $USE "
+    fi
 done <<< $DISKS
+
+echo 
