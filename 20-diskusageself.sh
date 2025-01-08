1c8cef9a81e69e02ad00604be17d9f1576e14cb3
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-08 19:16:45 +0530

    ok

--- 20-diskusageself.sh
+++ 20-diskusageself.sh
@@ -6,7 +6,7 @@ MSG=""
 
 while read -r line
 do 
-    USE=$(echo "$line" | awk -F " " '{print $6F}')
+    USE=$(echo "$line" | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PART=$(echo "$line" | awk -F " " '{print $NF}')
     echo "part: $PART , use: $USE"
      if [ $USE -ge $LIMIT ]
