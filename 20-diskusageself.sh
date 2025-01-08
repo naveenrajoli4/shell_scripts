e356ed0b8a422355a5cad183e15715242468d55c
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-08 19:08:17 +0530

    ok

--- 20-diskusageself.sh
+++ 20-diskusageself.sh
@@ -5,6 +5,8 @@ LIMIT=5
 
 while read -r line
 do 
-    echo "$line"
+    USE=$(echo "$line" | awk -F " " '{print $6F}')
+    PART=$(echo "$line" | awk -F " " '{print $NF}')
+    echo "use $USE , part: $PART"
 
 done <<< $DISKS
