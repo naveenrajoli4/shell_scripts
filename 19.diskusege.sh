d11e999d38f801b7abfeb7faa4203fbab083eaf7
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-08 18:50:36 +0530

    ok

--- 19.diskusege.sh
+++ 19.diskusege.sh
@@ -8,9 +8,9 @@ while read -r line
 do
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
-    echo "Partition: $PARTITION , Usage: $USAGE"
-    # if [ $USAGE -ge $DISK_THRESHOLD ]
-    # then
-    #     MSG+="High Disk usage on partition: $PARTITION Usage is: $USAGE \n "
-    # fi
+    #echo "Partition: $PARTITION , Usage: $USAGE"
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        MSG+="High Disk usage on partition: $PARTITION Usage is: $USAGE \n "
+    fi
 done <<< $DISK_USAGE
