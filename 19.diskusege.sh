6dc96de1544d072b0046d7049fab8b707390df17
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-08 18:49:50 +0530

    ok

--- 19.diskusege.sh
+++ 19.diskusege.sh
@@ -2,20 +2,15 @@
 
 DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=5 #real projects will monitor for 70
+MSG=""
 
 while read -r line
 do
-    USAGE=$(echo $line | awk -F " " '{print $6f}' | cut -d "%" -f1)
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk -F " " '{print $NF}')
     echo "Partition: $PARTITION , Usage: $USAGE"
-done <<< $DISK_THRESHOLD
-
-
-
-
-# while read -r line
-# do
-#     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-#     PARTITION=$(echo $line | awk -F " " '{print $NF}')
-#     echo "Partition: $PARTITION , Usage: $USAGE"
-# done <<< $DISK_USAGE
+    # if [ $USAGE -ge $DISK_THRESHOLD ]
+    # then
+    #     MSG+="High Disk usage on partition: $PARTITION Usage is: $USAGE \n "
+    # fi
+done <<< $DISK_USAGE
