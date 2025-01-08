cd5339ba3d9143d5419a06527bc0d8db84989d15
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-08 19:15:53 +0530

    ok

--- 20-diskusageself.sh
+++ 20-diskusageself.sh
@@ -11,8 +11,8 @@ do
     echo "part: $PART , use: $USE"
      if [ $USE -ge $LIMIT ]
      then
-        MSG=""High Disk usage on partition: $PART Usage is: $USE "
+        MSG+="High Disk usage on partition: $PART Usage is: $USE"
     fi
 done <<< $DISKS
 
-echo 
+echo -e "Message: $MSG"
