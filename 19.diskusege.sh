fd681d0995ec115b0748374dd83299894eb4d615
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-08 18:22:54 +0530

    ok

--- 19.diskusege.sh
+++ 19.diskusege.sh
@@ -5,7 +5,7 @@ THRESHOLD_LIMIT="5" #real projects will monitor for 70
 
 while read -r lines
 do
-    USEAGE=$(echo $lines | awk -f " " {print $6f} | cut -d "%" -f1)
+    USEAGE=$(echo $lines | awk -F " " {print $6f} | cut -d "%" -f1)
     PARTITION=$(echo $lines | awk -f " " {print $Nf})
     echo "partion : $PARTITION  uusahe :  $USEAGE "
 
