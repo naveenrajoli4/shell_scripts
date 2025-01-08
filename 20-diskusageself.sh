11ac710c1424c0835b8f7512699a13cdc1ae06db
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-08 19:17:21 +0530

    ok

--- 20-diskusageself.sh
+++ 20-diskusageself.sh
@@ -8,7 +8,7 @@ while read -r line
 do 
     USE=$(echo "$line" | awk -F " " '{print $6F}' | cut -d "%" -f1)
     PART=$(echo "$line" | awk -F " " '{print $NF}')
-    echo "part: $PART , use: $USE"
+    # echo "part: $PART , use: $USE"
      if [ $USE -ge $LIMIT ]
      then
         MSG+="High Disk usage on partition: $PART Usage is: $USE"
