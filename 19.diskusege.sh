dd82933cce0e4f3190e41f415d97c107a225a8c6
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-08 18:18:54 +0530

    ok

--- 19.diskusege.sh
+++ 19.diskusege.sh
@@ -4,7 +4,7 @@ DISK_USAGE=$(df -hT | grep xfs)
 THRESHOLD_LIMIT="5" #real projects will monitor for 70
 
 while read -r lines
-do 
-    echo $lines
+do
     USEAGE=$(echo $line | awk -F " " {print $6f} | cut -d "%" -f1)
+    
 done <<< $DISK_USAGE
