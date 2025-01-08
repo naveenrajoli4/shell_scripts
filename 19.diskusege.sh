b77a6d0f3348a755b153ed3b22593206f9a2ee90
naveenrajoli04 <naveenrajoli04@gmail.com>
2025-01-08 18:39:48 +0530

    ok

--- 19.diskusege.sh
+++ 19.diskusege.sh
@@ -1,21 +1,21 @@
 # #!/bin/bash
 
-DISK_USAGE=$(df -hT | grep xfs)
-DISK_THRESHOLD=5 #real projects will monitor for 70
-
-while read -r line
-do
-    USAGE=$(echo $line | awk -F " " '{print $6f}' | cut -d "%" -f1)
-    PARTITION=$(echo $line | awk -F " " '{print $Nf}')
-    echo "Partition: $PARTITION , Usage: $USAGE"
-done <<< $DISK_USAGE
-
 # DISK_USAGE=$(df -hT | grep xfs)
 # DISK_THRESHOLD=5 #real projects will monitor for 70
 
 # while read -r line
 # do
-#     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
-#     PARTITION=$(echo $line | awk -F " " '{print $NF}')
+#     USAGE=$(echo $line | awk -F " " '{print $6f}' | cut -d "%" -f1)
+#     PARTITION=$(echo $line | awk -F " " '{print $Nf}')
 #     echo "Partition: $PARTITION , Usage: $USAGE"
 # done <<< $DISK_USAGE
+
+DISK_USAGE=$(df -hT | grep xfs)
+DISK_THRESHOLD=5 #real projects will monitor for 70
+
+while read -r line
+do
+    USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | awk -F " " '{print $NF}')
+    echo "Partition: $PARTITION , Usage: $USAGE"
+done <<< $DISK_USAGE
